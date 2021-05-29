f8336ff84009138768a9fd751d8cba590bf3a684
Derek-Wallace <lacrosseid@gmail.com>
2021-05-29 16:30:39 -0600

    slight margin change

--- app/Models/Task.js
+++ app/Models/Task.js
@@ -9,7 +9,7 @@ export class Task{
 
   get template(){
     return`
-    <div class="row d-flex align-items-center mb-1"><input class="col-1" type="checkbox" name="completed" id="${this.id}" onclick="app.tasksController.updateTask('${this.id}')"><span class="col-8">${this.description}</span><i class="col-1 mdi mdi-trash-can" role="button" onclick="app.tasksController.removeTask('${this.id}')"></i></div>
+    <div class="row d-flex align-items-center mb-1 ml-1"><input class="col-1" type="checkbox" name="completed" id="${this.id}" onclick="app.tasksController.updateTask('${this.id}')"><span class="col-8">${this.description}</span><i class="col-1 mdi mdi-trash-can" role="button" onclick="app.tasksController.removeTask('${this.id}')"></i></div>
     `
   }
 }
