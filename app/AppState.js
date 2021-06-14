15390bf068f5cc10a85f4fa696ae0da1143b8ad2
Derek-Wallace <lacrosseid@gmail.com>
2021-06-14 10:18:58 -0600

    image fix?

--- app/AppState.js
+++ app/AppState.js
@@ -8,7 +8,7 @@ quote = null
 weather = null
 weatherUnit = 'F'
 tasks = []
-image = null
+image = 'https://lh3.googleusercontent.com/proxy/ELO93ZZQGz_M7pfqqZO6-FfcDRU28enmuzI1Yuh8YSE-6GCFbHEpFzJLB1YkyN7jW_Pxv9QfIq1v1T8lH66rErNoWorRNNXcrGyo4dg_dCgLaRvn5qBRdH3ym747eyKLxpQzVcehvLszooWGpfk'
 user = null
 }
 
