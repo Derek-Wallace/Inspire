c7e144e91ec345188442310b760b107953644615
Derek-Wallace <lacrosseid@gmail.com>
2021-06-14 10:35:24 -0600

    no doubt its fixed

--- app/main.js
+++ app/main.js
@@ -1,5 +1,5 @@
 import { ClockController } from "./Controllers/ClockController.js";
-import { ImageController } from "./Controllers/ImageController.js";
+// import { ImageController } from "./Controllers/ImageController.js";
 import { QuoteController } from "./Controllers/QuoteController.js";
 import { TasksController } from "./Controllers/TasksController.js";
 import { UserController } from "./Controllers/UserController.js";
@@ -10,7 +10,7 @@ class App {
 clockController = new ClockController()
 quoteController = new QuoteController()
 weatherController = new WeatherController()
-imageController = new ImageController()
+// imageController = new ImageController()
 tasksController = new TasksController()
 userController = new UserController()
 }
